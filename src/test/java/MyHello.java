040e27b257118f36285e9cd8110b270b55715e9c
shan <shanyulian@aliyun.com>
2017-02-06 13:07:14 +0800

    Initial commit

---
+++ src/test/java/MyHello.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created by shan on 2017/2/6.
+ */
+public class MyHello {
+
+    @Test
+    public  void myte(){
+        System.out.print("hello world!");
+//        Sys
+
+    }
+
+}
+
